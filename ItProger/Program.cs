7ec15fb4f2cece77d988ecc5b68a97b26b9a3f0b
krukd <krukd87@gmail.com>
2023-04-01 21:32:02 +0200

    Task 3

--- ItProger/Program.cs
+++ ItProger/Program.cs
@@ -13,9 +13,15 @@
                 это ваша переменная num.*/
 
 
-            var num = 20;
+            /*var num = 20;
             num += 5;
-            Console.WriteLine("Переменная: " + num);
+            Console.WriteLine("Переменная: " + num);*/
+
+
+            //Возведите число «1.4234» в степень 3.Округлите число к большему и выведите его на экран.
+
+            var num = 1.4234;
+            Console.WriteLine(Math.Ceiling(Math.Pow(num, 3)));
         }
     }
 }
