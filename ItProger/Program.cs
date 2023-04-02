852cc9a300da6aff8d5575fa6becfb1d365a84a4
krukd <krukd87@gmail.com>
2023-04-02 21:56:57 +0200

    Task 18

--- ItProger/Program.cs
+++ ItProger/Program.cs
@@ -279,13 +279,38 @@ namespace ItProger
             Функция должна вывести на экран массив в формате: «5 / 10». То есть 
             выводиться первый и последний элемент массива.*/
 
-            int[] array = { 5, 7, 8, 10 };
+            /*int[] array = { 5, 7, 8, 10 };
             getElementsOfArray(array);
 
         }
 
         public static void getElementsOfArray(int[] array) {
             Console.WriteLine(array[0] + " " + "/" + " " + array[array.Length-1]);
+        }*/
+
+            /*Создайте функцию «divideAndRound», что будет принимать в качестве 
+             * параметров три числа.
+
+            В функции проводите деление чисел.
+
+            Полученное число должно быть округлено к большему и возвращено из функции.
+
+            Выполните вызов функции, передачу в неё чисел «500.05, 2.25, 3.01» и вывод 
+            результата на экран в формате: «Результат: res», где res – это результат 
+            деления.
+
+            Подсказка: класс Math работает с типом данных double, поэтому переменные 
+            лучше создавать с этим типом.*/
+
+            Console.WriteLine("Результат: " + divideAndRound(500.05, 2.25, 3.01));
+        }
+
+        public static double divideAndRound(double a, double b, double c)
+        {
+            double resOfDivide = a / b / c;
+            double res = Math.Ceiling(resOfDivide);
+            return res;
+
         }
     }
 }
