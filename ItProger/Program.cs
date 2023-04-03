af54e0dfde29a01b1c7f5250976fff8faa75e0c1
krukd <krukd87@gmail.com>
2023-04-03 13:07:04 +0200

    Task 22

--- ItProger/Program.cs
+++ ItProger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ItProger
 {
@@ -35,14 +36,59 @@ namespace ItProger
             /*char[] chars = { 'J', 'a', 'v', 'a', '!' };
             Console.WriteLine("Элемент: " + chars[2]);*/
 
-            
-           /*Создайте цикл for от 1 до 5 включительно. В каждой итерации выводите на экран фразу: 
-           «Номер элемента: num», где num – это индекс каждой итерации.*/
 
-            for (int i = 1; i < 6; i++)
+            /*Создайте цикл for от 1 до 5 включительно. В каждой итерации выводите на экран фразу: 
+            «Номер элемента: num», где num – это индекс каждой итерации.*/
+
+            /*for (int i = 1; i < 6; i++)
             {
                 Console.WriteLine("Номер элемента: " + i);
-            }
+            }*/
+
+
+
+            /*Создайте массив «List». Добавьте в него элементы:
+
+            4, 7, 2, 8, 0, 3, 9
+            Найдите среднее арифметическое значение. Выведите на экран лишь те числа, 
+            что больше за среднее арифметическое.
+
+            Выводите в формате: «El: element», где element – значение элемента.*/
+
+            List<int> list = new List<int>();
+
+            list.Add(4);
+            list.Add(7);
+            list.Add(2);
+            list.Add(8);
+            list.Add(0);
+            list.Add(3);
+            list.Add(9);
+
+            /*double average = list.Average();
+
+            foreach (int i in list)
+            {
+                if (i > average)
+                {
+                    Console.WriteLine("El: " + i);
+                }
+            }*/
+
+            // Переменная для подсчета суммы
+            int summa = 0;
+            foreach (int el in list)
+                summa += el;
+
+            // Переменная для подсчёта среднего значения
+            float average = summa / list.Count;
+
+            foreach (int el in list)
+                if (el > average)
+                    Console.Write("El: " + el);
+
+
+
 
         }
     }
