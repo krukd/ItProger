2232bd96f10548d0eaa03242741401a2fecb11f9
krukd <krukd87@gmail.com>
2023-04-05 10:43:44 +0200

    Task 40

--- ItProger/Program.cs
+++ ItProger/Program.cs
@@ -9,146 +9,10 @@ namespace ItProger
     {
         static void Main(string[] args)
         {
-            /*Создайте программу, которая будет принимать число (n), введенное 
-             * пользователем, и выдавать результат в виде (n + n * 2).
-
-
-
-            Пример:
-
-            // Вводим 23
-            // Получаем: 2346*/
-
-            /*// Получаем число от пользователя
-            int n1 = int.Parse(Console.ReadLine());
-            // Вторая переменная содержит число от пользователя, умноженное на два
-            int n2 = n1 * 2;
-            // Выводим их вместе
-            // превращаем в строки, чтобы не добавлялись
-            Console.Write(n1.ToString() + n2.ToString());
             
-           Создайте переменные для хранения следующий значений:
-            -34, 4, 'Hello', 'R', 23.093433222, 30000, true, 0. 
+            //DepositCalculator();
 
-
-
-            Продумайте типы данных для переменных, чтобы они максимально подходили под 
-            каждое значение.
-
-
-            // Маленькое число, поэтому byte
-            byte num1 = 34;
-            byte num2 = 4;
-            // Строка
-            string word = "Hello";
-            // Символ
-            char sym = 'R';
-            // Много чисел после запятой, поэтому double
-            double num33 = 23.093433222f;
-            // Чуть больше число, поэтому short
-            short number = 30000;
-            // Булевое выражение
-            bool val = true;
-            // Байтовая переменная
-            byte simple = 0;
-
-            Попросите пользователя ввести:
-
-            имя
-            возраст
-            есть ли у него машина (true или false)
-
-
-            Выведите все значения, после их получения.
-
-            Console.WriteLine("Enter your name: ");
-
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter your age: ");
-
-            int age = int.Parse(Console.ReadLine());
-            Console.WriteLine("Do you have a car?");
-
-            string answer = Console.ReadLine();
-
-            bool yesOrNo = false;
-
-            if (answer == "yes")
-            {
-                yesOrNo = true;
-                //Console.WriteLine("You do have a car!");
-
-            } else
-            {
-                yesOrNo = false;
-                //Console.WriteLine("You do not have a car!");
-            }
-
-            
-            
-            Console.WriteLine("Your name is " + name);
-            Console.WriteLine("Your age is {0}", age);
-
-            if (yesOrNo)
-            {
-                Console.WriteLine("You do have a car!");
-            }
-            else
-            {
-                Console.WriteLine("You do not have a car!");
-            }
-            
-
-
-            Получите от пользователя данные и запишите их в переменные с типами данных:
-
-            short
-            ulong
-            char
-            double. 
-             
-             */
-
-            /*short a;
-            ulong b;
-            char s;
-            double db;
-*/
-            /*// Для конвертации в short испльзуйте ToInt16 (для int - ToInt32)
-            a = Convert.ToInt16(Console.ReadLine());
-            // Для конвертации в ulong испльзуйте ToUInt64
-            b = Convert.ToUInt64(Console.ReadLine());
-            // Для конвертации в char испльзуйте ToChar
-            s = Convert.ToChar(Console.ReadLine());
-            // Для конвертации в double испльзуйте ToDouble
-            db = Convert.ToDouble(Console.ReadLine());*/
-
-
-
-            /*Создайте переменную со значением 5.
-
-
-
-           *//* Выполните над ней операции:
-
-            умножьте её на 7;
-            выполните декременацию(уменьшение на один).*//*
-
-            int a1 = 5;
-            // Сокращенная форма умножения
-            a1 *= 7;
-            // Декременация
-            a1--;
-
-            Console.Write("Number is: " + a1);
-*/
-
-
-            //EnterNameAge();
-
-            //UrlAddress();
-
-            DepositCalculator();
+            Calcilator();
 
 
 
@@ -217,6 +81,51 @@ namespace ItProger
             }
 
             Console.WriteLine("После 5 лет ваш баланс будет составлять {0}", Math.Round(sum));
+        }
+
+
+        //Простой калькулятор
+        public static void Calcilator()
+        {
+            Console.WriteLine("Enter first number: ");
+
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+
+            int num2 = int.Parse(Console.ReadLine());
+
+            
+
+            Console.WriteLine("Выберите команду из списка ниже ");
+            Console.WriteLine("1. Добавить ");
+            Console.WriteLine("2. Отнять ");
+            Console.WriteLine("0. Ничего ");
+
+
+            
+
+            int sum = num1 + num2;
+
+            int substraction = num1 - num2;
+
+
+            int command = int.Parse(Console.ReadLine());
+            /*int command2 = int.Parse(Console.ReadLine());
+            int command3 = int.Parse(Console.ReadLine());*/
+            
+
+            if(command == 1)
+            {
+                Console.WriteLine("Результат сложения ваших чисел равен {0}", sum);
+            }else if(command == 2)
+            {
+                Console.WriteLine("Результат вычитания ваших чисел равен {0}", substraction);
+            }else { Console.WriteLine("Выберите номер операции, которую хотите произвести с вашими числами"); }
+
+
+
+
         }
 
     }
