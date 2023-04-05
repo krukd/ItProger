54c6652f3405011554d761e3a39dfee5c9c796c0
krukd <krukd87@gmail.com>
2023-04-05 12:24:47 +0200

    Task 43

--- ItProger/Program.cs
+++ ItProger/Program.cs
@@ -9,180 +9,72 @@ namespace ItProger
     {
         static void Main(string[] args)
         {
+            /*Небольшой калькулятор
+            Создайте программу, которая будет принимать 3 значения от пользователя и 
+            далее производить математическое действие (+, -, *, /). 
+
+
+
+            Например:
             
-            //DepositCalculator();
-
-            //Calcilator();
-
-            //EmptyString();
-
-            EnterThreeWords();
-
-
-
+             "Первое число: 2"
+            "Операция: -"
+            "Второе число: 3"
+            2 - 3 = -1
+            При делении на ноль должна выдаваться ошибка.*/
+            Calculator2();
 
         }
 
-        public static void EnterNameAge()
+        public static void Calculator2()
         {
 
-            // Создаем переменные с пустыми значениями
-            string name;
-            int age;
+            int result = 0;
+            Console.Write("Enter first number ");
+            int number1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введите ваше имя: ");
-            // Получаем целую строку от пользователя
-            name = Console.ReadLine();
+            Console.Write("Enter second number ");
+            int number2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введите ваш возраст: ");
-            // Получаем строку и конвертируем её в число
-            age = int.Parse(Console.ReadLine());
+            Console.Write("Какую оперцию хотите произвести? ");
+            char symbol = char.Parse(Console.ReadLine());
 
-            // Выводим данные на экран
-            Console.WriteLine("Привет, " + name + "! Тебе уже " + age);
-        }
+            /*Console.Write("Enter third number ");
+            int number3 = int.Parse(Console.ReadLine());*/
 
-        public static void UrlAddress() {
+            Console.WriteLine("Первое число: " + number1);
+            Console.WriteLine("Операция: " + symbol);
+            Console.WriteLine("Второе число: " + number2);
 
-
-            // Получаем веб сайт от пользователя
-            Console.Write("Enter website: ");
-            string website = Console.ReadLine();
-
-            // Разделяем слово по символу точка
-            string[] split = website.Split('.');
-            // Получиться массив состоящий из двух элементов
-            // Выводим в консоль последний элемент массива
-            Console.Write("Domain name: " + split[split.Length - 1]);
-        }
-
-        /*Сумма: 1250
-        Годы: 5
-        Процент: 12
-        1 год - 1400
-        2 год - 1568
-        3 год - 1756
-        4 год - 1966
-        После 4 лет сумма будет - 1966*/
-        public static void DepositCalculator()
-        {
-            Console.WriteLine("Введите сумму, которую хотите положить на счет: ");
-
-            double sum = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("На сколько лет хотите положить деньги? ");
-
-            int years = int.Parse(Console.ReadLine());
-
-            int persents = 12;
-
-            
-            for (int i = 1; i < years + 1; i++)
+            switch (symbol)
             {
-                sum += sum /100 * 12;
+                case '-': 
+                    Console.Write("Результат равен ");
+                    Console.Write( result = number1 - number2);
+                    break;
+                case '+':
+                    Console.Write("Результат равен ");
+                    Console.Write(result = number1 + number2);
+                    break;
+                case '*':
+                    Console.Write("Результат равен ");
+                    Console.Write(result = number1 * number2);
+                    break;
+                case '/':
 
-                Console.WriteLine(i + " year - " + Math.Round(sum));
+                    try
+                    {
+                        Console.Write("Результат равен ");
+                        Console.WriteLine(result = number1 / number2);
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Division by zero");
+                    }
+                    break;
             }
 
-            Console.WriteLine("После 5 лет ваш баланс будет составлять {0}", Math.Round(sum));
-        }
 
-
-        //Простой калькулятор
-        public static void Calcilator()
-        {
-            Console.WriteLine("Enter first number: ");
-
-            int num1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter second number: ");
-
-            int num2 = int.Parse(Console.ReadLine());
-
-            
-
-            Console.WriteLine("Выберите команду из списка ниже ");
-            Console.WriteLine("1. Добавить ");
-            Console.WriteLine("2. Отнять ");
-            Console.WriteLine("0. Ничего ");
-
-
-            
-
-            int sum = num1 + num2;
-
-            int substraction = num1 - num2;
-
-
-            int command = int.Parse(Console.ReadLine());
-            /*int command2 = int.Parse(Console.ReadLine());
-            int command3 = int.Parse(Console.ReadLine());*/
-            
-
-            if(command == 1)
-            {
-                Console.WriteLine("Результат сложения ваших чисел равен {0}", sum);
-            }else if(command == 2)
-            {
-                Console.WriteLine("Результат вычитания ваших чисел равен {0}", substraction);
-            }else { Console.WriteLine("Выберите номер операции, которую хотите произвести с вашими числами"); }
-
-        }
-
-        //Пустая строка
-        public static void EmptyString() {
-            // Изначально создаем строку с пустым значением
-            string user_input = "";
-            // Используем специальную функцию IsNullOrWhiteSpace,
-            // которая проверяет является ли строка пустой или содержит лишь пробелы.
-            // Цикл будет работать до тех пор, пока строка будет пустой
-            // или будет состоять из пробелов
-            while (String.IsNullOrWhiteSpace(user_input))
-            {
-                // Получаем строку от пользователя
-                Console.Write("Enter something: ");
-                user_input = Console.ReadLine();
-            }
-
-            // Выводим сообщение после цикла
-            Console.WriteLine("Hooray! You done that!");
-        }
-
-
-        /*Ввод данных с клавиатуры
-        Попросите пользователя ввести три слова через пробел. 
-
-
-
-        Если пользователь введет менее трех слов, то выполните получение данных 
-        повторно. 
-
-
-
-        Просите пользователя ввести данные до тех пор, пока не будет введено как 
-        минимум три слова через пробел.*/
-
-        public static void EnterThreeWords()
-        {
-
-            int count = 0;
-
-            while (count != 3)
-            {
-
-                Console.Write("Введите 3 слова через пробел ");
-                string userInput = Console.ReadLine();
-
-
-                 count = userInput.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Length;
-
-                if (count != 3)
-                {
-
-                    Console.WriteLine("Вы ввели {0} слов, а нужно ввести 3 слова через пробел", count);
-                }
-
-            }       
         }
 
 
