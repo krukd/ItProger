c783a08ec3918fe151a346ff73492a3f939baf05
krukd <krukd87@gmail.com>
2023-04-06 12:13:57 +0200

    Task 50

--- ItProger/Program.cs
+++ ItProger/Program.cs
@@ -8,38 +8,37 @@ namespace ItProger
     internal class Program
     {
         static void Main(string[] args)
+
+            
         {
-            /*Создайте двумерный массив и выведите его на экран при помощи циклов while.
 
+            int[] a = { 24, -63, 67, -12, 88, 94, -28, 82, 0, 53 };
+            int oneMinValue = a[0];
+            int twoMinValue = a[1];
 
-
-            Двумерный массив данных:
-
-                        int[,] x = { { 0, 34, 2 }, { 9, 12, 18 }, { 3, 4, 5 } };
-            */
-
-
-            int length = 3;
-
-            int[,] x = { { 0, 34, 2 }, { 9, 12, 18 }, { 3, 4, 5 } };
-
-            int i = 0;
-
-            while (i < length)
+            if (twoMinValue < oneMinValue)
             {
-                int j = 0;
-
-                while (j < length)
-                {
-                    Console.Write(x[i, j] + " ");
-                    j++;
-                }
-
-                i++;
+                int tmp = twoMinValue;
+                twoMinValue = oneMinValue;
+                oneMinValue = tmp;
             }
 
+            for (int i = 2; i < a.Length; i++)
+            {
+                if (twoMinValue > a[i])
+                {
+                    twoMinValue = a[i];
+                }
+                if (twoMinValue < oneMinValue)
+                {
+                    int tmp = twoMinValue;
+                    twoMinValue = oneMinValue;
+                    oneMinValue = tmp;
+                }
 
+            }
 
+            Console.WriteLine("min1 = {0}; min2 = {1}", oneMinValue, twoMinValue);
         }   
 
     }
