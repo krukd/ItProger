069da5e369d7fb58afeb63082788919088d4d53e
krukd <krukd87@gmail.com>
2023-04-07 22:20:40 +0200

    Task 51.3

--- ItProger/Program.cs
+++ ItProger/Program.cs
@@ -12,44 +12,47 @@ namespace ItProger
             
         {
 
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            int t = 0;
 
-            var color = Console.ReadLine();
-
-            switch (color)
+            do
             {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(t);
 
-                    Console.WriteLine("Your color is red!");
-                    break;
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
 
-                    Console.WriteLine("Your color is green!");
-                    break;
+                        Console.WriteLine("Your color is red!");
+                        break;
 
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
 
-                    Console.WriteLine("Your color is cyan!");
-                    break;
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
 
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
 
-                    Console.WriteLine("Your color is yellow!");
-                    break;
-            }
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
 
-
+                t++;
+            } while (t < 3);
         }
 
-        }
+    }
 }
 
     
