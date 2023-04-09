4692482557bb9df9ce2b7deba1bf9e839de48794
krukd <krukd87@gmail.com>
2023-04-09 17:56:45 +0200

    Task 52

--- ItProger/Program.cs
+++ ItProger/Program.cs
@@ -11,44 +11,33 @@ namespace ItProger
 
 
         {
-            //Задайте одномерный массив и напишите алгоритм, который находит в нем
-            //количество положительных чисел.
-            var arr = new int[] { 5, 6, -9, 1, 2, 3, -4 };
-
-            int count = 0;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] > 0)
-                {
-                    count++;
-                }
-            }
-
-            Console.WriteLine(count);
-
-
-            int[,] array = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-
-            /*int temp = 0;
-            foreach (var item in array) if (item > 0)
-                    temp++;
-            Console.WriteLine(temp);*/
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
 
             int temp = 0;
 
-            for (int i = 0; i <= array.GetUpperBound(0); i++) {
-            
-
-                for (int j = 0; j <= array.GetUpperBound(1); j++)
+            for(int i = 0;  i <= arr.GetUpperBound(0); i++)
+            {
+                for(int j = 0;  j <= arr.GetUpperBound(1); j++)
                 {
-                    if (array[i, j] > 0)
+                    for(int k = j + 1;  k <= arr.GetUpperBound(1); k++)
                     {
-                        temp++;
+                        if (arr[i, j] > arr[i, k])
+                        {
+                            temp = arr[i, k];
+                            arr[i, k] = arr[i, j];
+                            arr[i, j] = temp;
+                        }
                     }
+
+                    Console.Write(arr[i, j] + " ");
                 }
+                Console.WriteLine();
             }
 
-            Console.WriteLine(temp);
+
+            
+            
+
 
         }
     }
