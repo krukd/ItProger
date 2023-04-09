e18d3aab898c8567dc6b0e760a7f0d19a948b63f
krukd <krukd87@gmail.com>
2023-04-09 18:20:39 +0200

    Task 52.1

--- ItProger/Program.cs
+++ ItProger/Program.cs
@@ -11,32 +11,21 @@ namespace ItProger
 
 
         {
-            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            (string name, int age) anketa;
 
-            int temp = 0;
+            var (name, age) = ("Евгения", 27);
 
-            for(int i = 0;  i <= arr.GetUpperBound(0); i++)
-            {
-                for(int j = 0;  j <= arr.GetUpperBound(1); j++)
-                {
-                    for(int k = j + 1;  k <= arr.GetUpperBound(1); k++)
-                    {
-                        if (arr[i, j] > arr[i, k])
-                        {
-                            temp = arr[i, k];
-                            arr[i, k] = arr[i, j];
-                            arr[i, j] = temp;
-                        }
-                    }
+            Console.WriteLine("My name is {0}", name);
+            Console.WriteLine("My age is {0}", age);
 
-                    Console.Write(arr[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
+            Console.WriteLine("Enter your name ");
+            anketa.name = Console.ReadLine();
+            Console.WriteLine("Enter your age ");
+            anketa.age = int.Parse(Console.ReadLine());
 
 
-            
-            
+            Console.WriteLine("Your name is {0}", anketa.name);
+            Console.WriteLine("Your age is {0}", anketa.age);
 
 
         }
