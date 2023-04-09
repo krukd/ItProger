4f10dbea559e584739e582a25deb68ac0b045b7a
krukd <krukd87@gmail.com>
2023-04-09 16:56:20 +0200

    Task 51.8

--- ItProger/Program.cs
+++ ItProger/Program.cs
@@ -14,38 +14,44 @@ namespace ItProger
 
             var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
-            Array.Sort(arr);
-
-            foreach (int i in arr)
-            {
-                Console.Write(i + " ");
-            }
-
-            Console.WriteLine();
-
-
-            var arr1 = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-
-            int temp;
+            int sum = 0;
 
             for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[i] > arr[j])
-                    {
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                    }
-                }
+                sum += arr[i];
             }
 
-            foreach (var item in arr)
+            Console.WriteLine(sum);
+
+
+
+
+            //##################################
+
+
+            int[][] array = new int[3][];
+
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
+
+
+            foreach (var num in array)
             {
-                Console.Write(item + " ");
+                foreach (var item in num)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
             }
-
+            /*for (int i = 0;i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write(array[i][j] + " ");
+                }
+                Console.WriteLine();
+            }*/
         }
     }
 }
