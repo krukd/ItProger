27fb817b5844b3b828ff25b689bd70a9f3ce20f4
krukd <krukd87@gmail.com>
2023-04-09 16:00:16 +0200

    Task 51.4

--- ItProger/Program.cs
+++ ItProger/Program.cs
@@ -12,44 +12,22 @@ namespace ItProger
             
         {
 
-            int t = 0;
+            Console.WriteLine("Введите свое имя ");
+            string name = Console.ReadLine();
+            //Console.WriteLine(name);
+            Console.WriteLine("Ваше имя по буквам: ");
+            char [] array = name.ToArray();
 
-            do
+            for(int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine(t);
-
-                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-                switch (Console.ReadLine())
-                {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is red!");
-                        break;
-
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is green!");
-                        break;
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is cyan!");
-                        break;
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.WriteLine("Your color is yellow!");
-                        break;
-                }
-
-                t++;
-            } while (t < 3);
+                Console.Write(array[i] + " ");
+            }
+            /*foreach (char c in array)
+            {
+                Console.Write(c + " ");
+                
+            }*/
+            Console.Write(" Последняя буква вашего имени: " + array[array.Length - 1]);
         }
 
     }
