adcbcbebff7e113ffe88c80020568e642dada851
krukd <krukd87@gmail.com>
2023-04-12 22:39:46 +0200

    Task 52.9

--- ItProger/Program.cs
+++ ItProger/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
+using System.Reflection.PortableExecutable;
 
 namespace ItProger
 {
@@ -10,63 +12,29 @@ namespace ItProger
     {
         static void Main(string[] args)
 
-
+            
         {
-            Console.WriteLine("Enter your word: ");
 
+            Console.WriteLine(BreakCamelCase("camelCasing"));
+        }
 
+        public static string BreakCamelCase(string str)
+        {
+            // complete the function
+            string c = "";
 
-            //char [] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
-            string word = Console.ReadLine();
-
-            while (word != "exit")
+            for (int i = 0; i < str.Length; i++)
             {
-                char letter = word[0];
-                char letterToLowerCase = char.ToLower(letter);
-
-                if(letterToLowerCase >= 'a' && letterToLowerCase <= 'f' ) {
-                    FromAToF(word);
-                }else if(letterToLowerCase >= 'g' && letterToLowerCase <= 'l')
+                string a = char.ToString(str[i]);     
+                if (a.Equals(a.ToUpper()))
                 {
-                    FromGToL(word);
+                    c+= " ";
                 }
-                else if (letterToLowerCase >= 'm' && letterToLowerCase <= 'r')
-                {
-                    FromMToR(word);
-                }
-                else
-                {
-                    FromSToZ(word);
-                }
-
-                word = Console.ReadLine();
+                    c+= a;
             }
 
-
+            return c;
         }
-
-        public static void FromAToF(string word)
-        {
-            Console.WriteLine("FromAToF executes {0}", word);
-        }
-
-        public static void FromGToL(string word)
-        {
-            Console.WriteLine("FromGToL executes {0}", word);
-        }
-
-        public static void FromMToR(string word)
-        {
-            Console.WriteLine("FromMToR executes {0}", word);
-        }
-
-        public static void FromSToZ(string word)
-        {
-            Console.WriteLine("FromSToZ executes {0}", word);
-        }
-
-
     }
 }
 
